57f3c9efdb32e7a5021893716dd9a76f9189ca25
ktk1975 <ktk1975@wemakeprice.com>
2019-11-08 11:00:54 +0900

    first success

--- script/codegen.sh
+++ script/codegen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./generate-groups.sh all "github.com/superman/demo/pkg/client" "github.com/superman/demo/pkg/apis" foo:v1
+./generate-groups.sh all "github.com/taekyung-kim-kr/demo/pkg/client" "github.com/taekyung-kim-kr/demo/pkg/apis" foo:v1 --go-header-file ./hack/boilerplate.go.txt
